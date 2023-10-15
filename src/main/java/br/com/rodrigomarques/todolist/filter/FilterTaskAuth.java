3b0e3e5f9820a2693212329834f4cf98bcf370a4
RodrigoMarq99 <rodrigo.marquesoliveira@hotmail.com>
2023-10-15 16:24:18 -0300

    My personal task manager with Spring Boot

--- src/main/java/br/com/rodrigomarques/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/rodrigomarques/todolist/filter/FilterTaskAuth.java
@@ -27,7 +27,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
             throws ServletException, IOException {
         
                 var servletPath = request.getServletPath();
-                if(servletPath.equals("/tasks/")){
+                if(servletPath.startsWith("/tasks/")){
                     // Pegar a autentificação (usuário e senha)
                     var authorization = request.getHeader("Authorization");
                     var authEncoded = authorization.substring("Basic".length()).trim();
